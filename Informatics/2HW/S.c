a1e5604b75a48cc255a99723f70d70e51ad8b89c
Kablys <mfrohd@gmail.com>
2014-03-30 22:42:43 +0300

    Output to html

--- Informatics/2HW/S.c
+++ Informatics/2HW/S.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+void beginHtml(FILE *html){
+	fprintf(html, "<!DOCTYPE HTML> \n");
+	fprintf(html, "</html> \n");
+	fprintf(html, "   <head> \n");
+	fprintf(html, "      <title>Java Syntax</title> \n");
+	fprintf(html, "	<link href=""special.css"" rel=""stylesheet"" type=""text/css""> \n");
+	fprintf(html, "   </head> \n");
+	fprintf(html, "<body> \n");
+	fprintf(html, "<pre>\n");
+}
+
 int main(){
 	int	c;
-	FILE *data;
+	FILE *data,
+		 *html;
 	data = fopen("Code.java", "r");
+	html = fopen("index.html", "w");
+	beginHtml(html);
 	if (data) {
 		while ((c = getc(data)) != EOF)
-			putchar(c);
+			putc(c,html);
 		fclose(data);
 	}
 	return 0;
