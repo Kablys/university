72af824a6a67865b7ca1bf90a9c7b327e71c00db
Kablys <mfrohd@gmail.com>
2014-03-30 18:36:53 +0300

    prints from file to terminal

--- Informatics/2HW/S.c
+++ Informatics/2HW/S.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main(){
-	
+	int	c;
+	FILE *data;
+	data = fopen("Code.java", "r");
+	if (data) {
+		while ((c = getc(data)) != EOF)
+			putchar(c);
+		fclose(data);
+	}
 	return 0;
 }
