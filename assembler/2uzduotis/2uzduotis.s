a1079f7d20aa848597c26f88f7228beba5786efb
Ablingis Dominykas <dominykas.ablingis@mif.stud.vu.lt>
2014-12-17 00:43:01 +0200

    extended asm spliter to 10000 file max

--- assembler/2uzduotis/2uzduotis.s
+++ assembler/2uzduotis/2uzduotis.s
@@ -12,33 +12,26 @@
 %endmacro
 
 section .data 
-	main db 'Main.txt', 0
-	mainLen equ $ - main
-
-	half1 db 'half1.txt', 0
-	half1len equ $ - half1
-
-	half2 db 'half2.txt', 0
-	half2Len equ $ - half2
-
-	output db '0123456789abcdefghijk', 0xa
-	outputLen equ $ - output
-
 	helpMsg db '2run [SIZE] [FILENAME] ', 0xa
-	helpMsgLen equ $ - main
+	helpMsgLen equ $ - helpMsg
 
-	mag db 10
+	magnitude db 10
 
 section .bss
-	fd_out resb 4 	;reserve one byte for output file descriptor	
-	fd_in  resb 4 	;reserve one byte for input file descriptor
-	size   resb 4 	;holds number of into how many parts can file be sdbabplit
+;	magnitude db 1
+
+	fd_out resb 4 	;reserve four byte for output file descriptor	
+	fd_in  resb 4 	;reserve four byte for input file descriptor
+
+	size   resb 4 	;holds number of into how many parts can file be split
+	
 	mainName   resb 255 ;max filename length
 	mainNamelen   resb 1 ;max filename length
 	partName   resb 255 ;max filename length
+
 	fileNum resb 1
-	amount resb 1;
-	text   resb 10
+	amount resb 1
+	;text   resb 10
 	buff   resb 4096 ; hardware block size for my hard drive
 	;/sys/block/sda/queue/physical_block_size
 
@@ -46,50 +39,48 @@ section	.text
 		global _start 	  ; must be declared for linker
 
 _start:
-;		mov size, 10 	;TODO remove
 		pop ebx; 		;argc
 		cmp ebx, 3 		;if 1-filename, 2-first arg, 3-second arg
-		jne printHelp
-		jmp begin
+		jne printHelp 	;if not enoth arguments
+		jmp beginProgram
 		
 printHelp:
 		WRITE_STRING helpMsg, helpMsgLen		
 		jmp done;
 
-begin:
+beginProgram:
 		pop ebx 		;we ignore this argv[0] - programs name
 		pop edx 		;first argumet should be number so we convert it
 		xor eax, eax
-top:
+ConvertFromASCIItoInt:
 		movzx ecx, byte [edx] ;movzx nukopijuoja o likusius bitus nunulina
 		inc edx
 		cmp ecx, '0' 
-		jb bottom
+		jb LastNumber
 		cmp ecx, '9'
-		ja bottom
+		ja LastNumber
 		sub ecx, '0'
 		imul eax, 10
 		add eax, ecx
-		jmp top
-bottom:
+		jmp ConvertFromASCIItoInt
+LastNumber:
 		mov dword [size], eax
-stop:
+
+;Get filename
 		pop esi 		;File name for spliting
 		mov edi, mainName
 		mov ecx, 0
-		startas:
+		CopyNextChar:
 		cmp byte [esi], 0
-		je kitas
+		je LastChar
 		movsb
 		inc ecx
-		jmp startas
+		jmp CopyNextChar
 
-kitas:
+LastChar:
 		mov [mainNamelen], ecx
 		mov al, 0
 		stosb
-
-;create Main.txt file that gona be split
 
 ; open Main file
 		mov eax, 5
@@ -101,37 +92,38 @@ kitas:
 
 
 		mov byte [fileNum], 0
+		
 readFromMainFile:
 		mov eax, 3 		;sys_read
 		mov ebx, [fd_out]
-		mov ecx, text
-		mov edx, 10
+		mov ecx, buff
+		mov edx, [size]
 		int 80h
+
 		inc byte [fileNum]
 		cmp byte[fileNum], 255 	;length of text buffer
 		je done
 		cmp eax, [size] 	;length of text buffer
-		je create
+		je calculateNumber
 		jmp done
 
 ;testIfEnd;
 		
-create:
+calculateNumber:
 		mov edi, partName
-		mov ecx, 3
-		mov bh, 100
-		;mov byte[amount], 3
-		movzx ax, byte [fileNum]
-		mov byte [mag], 10
+		mov ecx, 5
+		mov bx, 10000 	; Divisor (max value of bx = 65535
+		movzx ax, byte [fileNum] ; Dividend
 getNumber:
-		div bh
-		add al, '0'
-		stosb
-		mov dh, ah
-		movzx ax, bh
-		div byte 10
-		mov bh, al
-		movzx ax, dh
+		div bx
+		;cmp
+		add ax, '0'
+		stosw 	;From AX -> DI
+		;mov dh, ah
+		mov ax, bx 	; put Divisor to ax register
+		div byte [magnitude] ; divide Divisor by 10
+		mov bx, ax
+		mov ax, dx
 		loop getNumber
 		
 		movzx ecx, byte [mainNamelen]
@@ -141,7 +133,7 @@ getName:
 		loop getName
 
 makeFile:
-		mov al, 0
+		mov al, 0 ;File names must end with \0 in Linux
 		stosb
 
 		mov eax, 8 		;sys_create
@@ -153,7 +145,7 @@ makeFile:
 ; write into new file
 		mov eax, 4
 		mov ebx, [fd_in]
-		mov ecx, text
+		mov ecx, buff
 		mov edx, [size]
 		int 80h
 
@@ -167,7 +159,7 @@ finalWrite:
 		mov  [size], eax
 ; create first file for split
 		mov eax, 8 		;sys_create
-		mov ebx, half2 	;name of the file
+		mov ebx, partName 	;name of the file
 		mov ecx, 0666q 	;read, write, all
 		int 80h
 		mov [fd_in], eax
@@ -175,7 +167,7 @@ finalWrite:
 ; write into new file
 		mov eax, 4
 		mov ebx, [fd_in]
-		mov ecx, text
+		mov ecx, buff
 		mov edx, [size]
 		int 80h
 
@@ -197,13 +189,13 @@ done:	mov	eax,1	  ; specify sys_exit
 ; read from file
 		mov eax, 3 		;sys_read
 		mov ebx, [fd_out]
-		mov ecx, text
+		mov ecx, buff
 		mov edx, 10
 		int 80h
 
 ; create second file for split
 		mov eax, 8 		;sys_create
-		mov ebx, half2 	;name of the file
+		mov ebx, buff 	;name of the file
 		mov ecx, 0666q 	;read, write, by all
 		int 80h
 		;sekmingai atliku operacija eax laiko nuoroda i sukurta faila
@@ -212,7 +204,7 @@ done:	mov	eax,1	  ; specify sys_exit
 ; write into new file
 		mov eax, 4
 		mov ebx, [fd_in]
-		mov ecx, text
+		mov ecx, buff
 		mov edx, 10
 		int 80h
 
