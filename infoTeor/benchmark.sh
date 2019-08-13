69bdbd65ce88167db9f2cde66238befff9b666a3
Kablys <mfrohd@gmail.com>
2019-08-13 23:12:37 +0300

    Last push

--- infoTeor/benchmark.sh
+++ infoTeor/benchmark.sh
@@ -3,6 +3,9 @@ echo Benchmarking $1 compression
 if [ $1 = "mod" ]; then
 	min=2
 	max=9
+elif [ $1 = "lzw.rb" ]; then
+	min=9
+	max=16
 else
 	echo "error with $1 command"
 	exit
@@ -17,11 +20,12 @@ for file in testFiles/* ; do
 	printf "$(stat --printf="%s" $file)\t"
 	for size in $(seq $min $max); do
 		tmpfile=$(mktemp)
-		#echo "$size $file $tmpfile"
-		./$1 en $size $file $tmpfile > /dev/null
+		#./$1 en $size $file $tmpfile > /dev/null
+		ruby $1 en $size $file $tmpfile > /dev/null
 		printf "$(stat --printf="%s" $tmpfile)\t"
 		tmpfile2=$(mktemp)
-		./$1 de $tmpfile $tmpfile2 > /dev/null
+		#./$1 de $tmpfile $tmpfile2 > /dev/null
+		ruby $1 de $tmpfile $tmpfile2 > /dev/null
 		diff $file $tmpfile2	
 		#echo $?
 		if [ "$?" -eq 0 ]; then
